aaaecc1a35a3ce6b30667187e1f5ab37ef97b22e
KK-2495 <krishnankalimuthunadar@hotmail.com>
2023-04-19 12:50:54 +0530

    updated

---
+++ userData.js
@@ -0,0 +1,21 @@
+function login(event) {
+    event.preventDefault();
+    var email = document.getElementById("userEmail").value;
+    var password = document.getElementById("userPassword").value;
+    // alert(email,password);
+    var LSlogin = JSON.parse(localStorage.getItem("Users"));
+    console.log(LSlogin);
+    var flagForUser = false;
+    for(var i=0; i<LSlogin.length; i++){
+      // console.log(LSlogin[i].userEmail);
+      if(LSlogin[i].userEmail == email && LSlogin[i].userPassword == password){
+        flagForUser = true;
+      }
+    }
+    if(flagForUser == true){
+      alert("Registration Successful.")
+    }else{
+      alert("Credential does not match")
+    }
+  
+  }
