e19dfa12f2da0948c8f3cf178d8f2ff54341f562
KK-2495 <krishnankalimuthunadar@hotmail.com>
2023-05-03 12:54:03 +0530

    Added Logout Functionality

--- userData.js
+++ userData.js
@@ -5,42 +5,44 @@ function register(event) {
   // console.log(name);
   var email = document.getElementById("userEmail").value;
   var password = document.getElementById("userPassword").value;
-  var confirmPassword = document.getElementById("userConfirmPassword").value;  
+  var confirmPassword = document.getElementById("userConfirmPassword").value;
 
-
-
-  if(name && email && password && confirmPassword){
-    if(password.length >= 8 && confirmPassword.length >= 8){
-      if(password == confirmPassword){
+  if (name && email && password && confirmPassword) {
+    if (password.length >= 8 && confirmPassword.length >= 8) {
+      if (password == confirmPassword) {
         // console.log(userInfo);
-        
+
         var multiUsers = JSON.parse(localStorage.getItem("hotStarUsers")) || [];
         var flagForEmail = false;
-        
-        for(var i = 0; i<multiUsers.length; i++){
-          if(multiUsers[i].hStarEmail == email){
+
+        for (var i = 0; i < multiUsers.length; i++) {
+          if (multiUsers[i].hStarEmail == email) {
             flagForEmail = true;
           }
         }
-        if(!flagForEmail){
-          var userInfo = {hStarUser: name, hStarEmail: email, hStarPass: password, hStarConfirmPass: confirmPassword};
+        if (!flagForEmail) {
+          var userInfo = {
+            hStarUser: name,
+            hStarEmail: email,
+            hStarPass: password,
+            hStarConfirmPass: confirmPassword,
+          };
           multiUsers.push(userInfo);
           localStorage.setItem("hotStarUsers", JSON.stringify(multiUsers));
           alert("Registered Successful.");
           window.location.href = `./Login.html`;
-        }else{
-          alert("You're Already Registered with this Email")
+        } else {
+          alert("You're Already Registered with this Email");
         }
-      }else{
-        alert("Passwords doesn't Match.")
+      } else {
+        alert("Passwords doesn't Match.");
       }
-    }else{
+    } else {
       alert("Passwords must be more than Eight");
     }
-  }else{
+  } else {
     alert("All fields are mandatory.");
   }
-
 }
 
 // ************Login Function***********//
@@ -52,29 +54,32 @@ function login(event) {
   // console.log(name);
   var password = document.getElementById("userPassword").value;
 
-  
   var hStarLogin = JSON.parse(localStorage.getItem("hotStarUsers"));
   // console.log(hStarLogin);
-  
+
   var flagForEmail = false;
   var currentUser;
-  for(var i=0; i<hStarLogin.length; i++){
+  for (var i = 0; i < hStarLogin.length; i++) {
     // console.log(hStarLogin[i].hStarEmail);
-    if(hStarLogin[i].hStarEmail == email && hStarLogin[i].hStarPass == password){
+    if (
+      hStarLogin[i].hStarEmail == email &&
+      hStarLogin[i].hStarPass == password
+    ) {
       // console.log(hStarLogin[i]);
       flagForEmail = true;
       currentUser = hStarLogin[i];
     }
-  }if(flagForEmail == true){  
+  }
+  if (flagForEmail == true) {
     // console.log(currentUser);
     localStorage.setItem("hStarActiveUser", JSON.stringify(currentUser));
-    alert("Logged IN ")
+    alert("Logged IN ");
     window.location.href = `./HomePage.html`;
-  }else{
-    alert("Please Register to Login..")
+  } else {
+    alert("Please Register to Login..");
   }
-
-  
 }
 
+
+// *******Log out******
 
